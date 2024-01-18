a185aeef5d5bc51632cfd38335f093877fbc2f51
rajushar <rajushar@host.docker.internal>
2024-01-18 20:30:02 +0530

    first commit

---
+++ shreemanancareercenter/src/test/java/com/shreemanancareercenter/ShreemanancareercenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreemanancareercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShreemanancareercenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
